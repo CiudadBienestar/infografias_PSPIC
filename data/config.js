a01350e2fb70ef077a64ef08c23d841681c64c50
CiudadBienestar <acsistematizacion@gmail.com>
2025-06-14 17:46:09 -0500

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,7 @@
-// Configuración de columnas y pestañas
+
+const GOOGLE_SHEET_ID = '1srJaMCHuNcwcKVyLbKOAREf03BNn88jeSkD5qyvN42E';
+
+const SHEET_NAMES = {
+  acciones: 'Acciones Masivas e Informativas',
+  procesos: 'Procesos Formativos'
+};
