a0883ed5e7a1baff44e288f7bb588edd8708ed89
CiudadBienestar <acsistematizacion@gmail.com>
2025-06-15 00:01:38 -0500

    Update and rename config.js to dashboard.js

--- data/config.js
+++
@@ -1,7 +0,0 @@
-
-const GOOGLE_SHEET_ID = '1srJaMCHuNcwcKVyLbKOAREf03BNn88jeSkD5qyvN42E';
-
-const SHEET_NAMES = {
-  acciones: 'Acciones Masivas e Informativas',
-  procesos: 'Procesos Formativos'
-};
